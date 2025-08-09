fed768f0eca9b30ad677e0642b2bb4226368923e
David Mannes <git@davidmannes.de>
2025-08-09 16:12:51 +0200

    Allow adding new items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,21 @@ function App() {
 
   const list = listItems.map(item => <li>{item}</li>)
 
+  const [newItem, setNewItem] = useState("")
+
+  const addItem = () => {
+      setListItems([...listItems, newItem]);
+      setNewItem("");
+  }
+
   return (
     <>
       <h1>ShopList</h1>
       <ul>
         {list}
       </ul>
+      <input type='text' value={newItem} onChange={e => setNewItem(e.target.value)}/>
+      <button onClick={addItem}>Add</button>
     </>
   )
 }
