57db07476e15e5b87234fb8c5fccc1ac2c274039
David Mannes <git@davidmannes.de>
2025-08-09 15:58:48 +0200

    Add list with dummy items

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [listItems, setListItems] = useState(["Milk", "Oats", "Onions"])
+
+  const list = listItems.map(item => <li>{item}</li>)
+
   return (
     <>
       <h1>ShopList</h1>
+      <ul>
+        {list}
+      </ul>
     </>
   )
 }
