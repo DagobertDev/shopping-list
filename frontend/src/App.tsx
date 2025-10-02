a99cc7a61d802a902160f59491cfc4949fcf3faf
David Mannes <git@davidmannes.de>
2025-10-02 13:49:26 +0200

    Add 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
 import ItemList from './components/ItemList'
+import NotFound from './components/NotFound'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ItemList />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
