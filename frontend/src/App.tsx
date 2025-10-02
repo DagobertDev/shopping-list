a0c1623a7900a62607121b93a3b4e6d0a7d944cf
David Mannes <git@davidmannes.de>
2025-10-02 14:10:42 +0200

    Add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,19 @@ import { Link, Route, Routes } from 'react-router'
 import './App.css'
 import ItemList from './components/ItemList'
 import NotFound from './components/NotFound'
+import Login from './components/Login'
 
 function App() {
   return (
     <>
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/Login">Login</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<ItemList />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
